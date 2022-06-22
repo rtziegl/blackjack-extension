ec546e8eecb71ef990ee4de637ee609c9e865f2a
Ryley Ziegler <ryleyziegler@Ryleys-MacBook-Pro.local>
2022-06-22 09:19:50 -0400

    Clean up and jquery implementation.

--- BJ Strategy/load.js
+++ BJ Strategy/load.js
@@ -1,16 +1,21 @@
-const LOAD_TIMEOUT = setTimeout(showPage, 2000);
+const LOAD_TIMEOUT = setTimeout(showPage, 1700);
 var seconds = 0;
 
+
 function showPage() {
-  document.getElementById("loading_page").style.display = "none";
+  $(document).ready(function(){
+    $(".loading_screen").fadeOut();
+  });
+  //document.getElementById("loading_page").style.display= "none";
   document.getElementById("app_window").style.display = "block";
 }
 
+/*
 function incrementSeconds(){
   seconds += 1;
 }
 
-/*chrome.windows.onFocusChanged.addListener(function(window) {
+chrome.windows.onFocusChanged.addListener(function(window) {
     var cancel = setInterval(incrementSeconds, 1000);
     if (cancel > 160){
       document.getElementById("loading_page").style.display = "block";
